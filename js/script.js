9329241270a73f531e4dce7d0b9943f499bd17c4
aleksanchez <sfloriana8@gmail.com>
2022-03-21 13:11:59 -0700

    Updated game to dosplay the word and the guessed letter

--- js/script.js
+++ js/script.js
@@ -28,7 +28,6 @@ guessLetterButton.addEventListener("click", function (e) {
     const guess = letterInput.value;
     const goodGuess = validateInput(guess);
     if (goodGuess) {	
-        // We've got a letter! Let's guess!	
         makeGuess(guess);	
       }	
       letterInput.value = "";	
@@ -55,10 +54,41 @@ const makeGuess = function(guess){
     } else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        showGuessedLetters();
+        updateWordInProgress(guessedLetters);
     }
 };
 
 
+const showGuessedLetters = function () {
+    guessedLettersElement.innerHTML = "";
+    for (const letter of guessedLetters) {
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessedLettersElement.append(li);
+    }
+};
 
+const updateWordInProgress = function(guessedLetters){
+    var wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    const revealWord = [];
+    for (const letter of wordArray) {
+        if (guessedLetters.includes(letter)) {
+            revealWord.push(letter.toUpperCase());
+        } else {
+            revealWord.push("●");
+        }
+    }
+    wordInProgress.innerText = revealWord.join("");
+    checkIfWin();
+}
+
+const checkIfWin = function() {
+    if (word.toUpperCase() === wordInProgress.innerText){
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">You guessed correct the word! Congrats!</p>`
+    }
+};
 
 
