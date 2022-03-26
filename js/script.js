2b38293ed41c256bebd38053dba05f2d79553059
aleksanchez <sfloriana8@gmail.com>
2022-03-26 11:49:45 -0700

    Finalized functioning part of game

--- js/script.js
+++ js/script.js
@@ -5,11 +5,27 @@ const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuessesElement = document.querySelector(".remaining");
 const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
-const playAgainButton = document.querySelector(".play-again ");
+const playAgainButton = document.querySelector(".play-again");
 
-const word = "magnolia";
+let word = "magnolia";
 const guessedLetters = [];
+let remainingGuesses = 8;
 
+const getWord = async function() {
+    const response = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+    const words = await response.text();
+    const wordArray = words.split("\n");
+    const randomIndex = Math.floor(Math.random() *wordArray.length);
+    word = wordArray[randomIndex].trim();
+    placeholder(word);
+
+    //console.log(wordArray);
+};
+getWord();
+
+
+
+//Starts the game
 
 const placeholder = function(word){
     const placeholderLetters = [];
@@ -20,13 +36,15 @@ const placeholder = function(word){
     wordInProgress.innerText = placeholderLetters.join("");
 };
 
-placeholder(word);
 
+	
 guessLetterButton.addEventListener("click", function (e) {
     e.preventDefault();
     message.innerText = "";
     const guess = letterInput.value;
     const goodGuess = validateInput(guess);
+    
+    
     if (goodGuess) {	
         makeGuess(guess);	
       }	
@@ -37,7 +55,7 @@ guessLetterButton.addEventListener("click", function (e) {
 const validateInput = function (input) {
     const acceptedLetter = /[a-zA-Z]/;
     if (input.length === 0) {
-        message.innerText = "Please only type one letter.";
+        message.innerText = "Please enter a letter.";
     } else if (input.length > 1) {
         message.innerText= "Please don't type more than one letter";
     } else if (!input.match(acceptedLetter)) {
@@ -47,13 +65,14 @@ const validateInput = function (input) {
     }
 };
 
-const makeGuess = function(guess){
+const makeGuess = function (guess){
     guess = guess.toUpperCase();
     if (guessedLetters.includes(guess)) {
         message.innerText = "You already guessed that letter. Try again";
     } else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        updateGuessesRemaining(guess);
         showGuessedLetters();
         updateWordInProgress(guessedLetters);
     }
@@ -70,7 +89,7 @@ const showGuessedLetters = function () {
 };
 
 const updateWordInProgress = function(guessedLetters){
-    var wordUpper = word.toUpperCase();
+    const wordUpper = word.toUpperCase();
     const wordArray = wordUpper.split("");
     const revealWord = [];
     for (const letter of wordArray) {
@@ -84,11 +103,28 @@ const updateWordInProgress = function(guessedLetters){
     checkIfWin();
 }
 
+const updateGuessesRemaining = function (guess){
+    const upperWord = word.toUpperCase();
+    if (!upperWord.includes(guess)) {
+        message.innerText = `Sorry the word has no ${guess}.`;
+        remainingGuesses -= 1;
+    } else {
+        message.innerText= `Good guess! The word has the letter ${guess}.`;
+    }
+    if (remainingGuesses === 0){
+        message.innerHTML = `The game is over. The word was <span class="highlight">${word}</span>.`
+    } else if (remainingGuesses === 1){
+        remainingGuessesSpan.innerText = `${remainingGuesses} guess`;
+    } else {
+        remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
+    }
+}
+
 const checkIfWin = function() {
     if (word.toUpperCase() === wordInProgress.innerText){
         message.classList.add("win");
-        message.innerHTML = `<p class="highlight">You guessed correct the word! Congrats!</p>`
-    }
+        message.innerHTML = `<p class="highlight">You guessed correct the word! Congrats!</p>`;
+    }  
 };
 
 
